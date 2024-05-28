47629ed2ac7eb7ac90f5cb1d5e786a80e3575ed1
Anmewzaa <punyakon857@gmail.com>
2024-05-28 22:08:29 +0700

    Create protected_menu.js

---
+++ project/routes/menu_route/protected_menu.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const {} = require("../../controllers/menu_controller");
+const { upload } = require("../../middlewares/multer");
+
+// CREATE
+
+// UPDATE
+
+// DELETE
+
+module.exports = router;
