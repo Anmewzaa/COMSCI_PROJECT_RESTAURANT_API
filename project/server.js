2b20a7d39b548f7c7444c463a58386256f08641f
Anmewzaa <punyakon857@gmail.com>
2024-05-28 22:08:21 +0700

    Update server.js

--- project/server.js
+++ project/server.js
@@ -2,21 +2,48 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const passport = require("passport");
 require("dotenv").config();
+require("./config/passport")(passport);
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-
+app.use(passport.initialize());
+app.use("/images", express.static("images"));
+// app.use(passport.session());
 mongoose
   .connect(process.env.DATABASE)
   .then(() => console.log(`Connected to database successfully`))
   .catch((err) => console.log(`Error : ${err}`));
 
+// unprotected
 app.use("/auth", require("./routes/auth_route"));
-app.use("/user", require("./routes/user/user_route"));
+app.use("/option", require("./routes/option_route/unportected_option_route"));
+app.use(
+  "/category",
+  require("./routes/category_route/unprotected_category_route")
+);
+app.use("/menu", require("./routes/menu_route/unprotected_menu"));
+// protected
+app.use(
+  "/user",
+  passport.authenticate("jwt", { session: false }),
+  require("./routes/user_route/user_route")
+);
+app.use(
+  "/option",
+  passport.authenticate("jwt", { session: false }),
+  require("./routes/option_route/protected_option_route")
+);
+app.use(
+  "/category",
+  passport.authenticate("jwt", { session: false }),
+  require("./routes/category_route/protected_category_route")
+);
 
 const port = process.env.PORT;
 app.listen(port, "0.0.0.0", () => {
