f50f2463872d71c88c96ff203bfb392097a871b1
Anmewzaa <punyakon857@gmail.com>
2024-05-28 22:08:24 +0700

    Delete user_route.js

--- project/routes/user/user_route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { get, create } = require("../../controllers/user/user_controller");
-
-router.get("/get", get);
-router.post("/create", create);
-
-module.exports = router;
