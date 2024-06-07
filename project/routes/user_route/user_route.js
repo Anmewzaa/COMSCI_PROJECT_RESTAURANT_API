5d92dd28ef81913b325fa8d8809a590d6f2a82be
Anmewzaa <punyakon857@gmail.com>
2024-06-07 22:21:23 +0700

    Update user_route.js

--- project/routes/user_route/user_route.js
+++ project/routes/user_route/user_route.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { get, create, update } = require("../../controllers/user_controller");
+const {
+  get_user,
+  getone_user,
+  create_user,
+  update_user,
+  delete_user,
+} = require("../../controllers/user_controller");
 
-router.get("/get", get);
-router.post("/create", create);
-router.put("/update/:user_id", update);
+router.get("/get", get_user);
+router.get("/get/:user_id", getone_user);
+router.post("/create", create_user);
+router.put("/update/:user_id", update_user);
+router.delete("/delete/:user_id", delete_user);
 
 module.exports = router;
