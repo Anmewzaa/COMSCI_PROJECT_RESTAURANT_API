3d2316ead93fce654659393d19e23c075bb77f27
Anmewzaa <punyakon857@gmail.com>
2024-06-07 17:48:18 +0700

    Update unprotected_category_route.js

--- project/routes/category_route/unprotected_category_route.js
+++ project/routes/category_route/unprotected_category_route.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { get_category } = require("../../controllers/category_controller");
+const {
+  get_category,
+  getone_category,
+} = require("../../controllers/category_controller");
 
 // GET
 router.get("/get", get_category);
+router.get("/get/:category_id", getone_category);
 
 module.exports = router;
