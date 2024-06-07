0e96761db6d624497bd6a004d5b8d90e538bdc84
Anmewzaa <punyakon857@gmail.com>
2024-06-07 17:48:26 +0700

    Create zone_route.js

---
+++ project/routes/zone_route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  get_zone,
+  create_zone,
+  update_zone,
+  delete_zone,
+} = require("../controllers/zone_controller");
+
+// GET
+router.get("/get", get_zone);
+// CREATE
+router.post("/create", create_zone);
+// UPDATE
+router.put("/update/:zone_id", update_zone);
+// DELETE
+router.delete("/delete/:zone_id", delete_zone);
+
+module.exports = router;
