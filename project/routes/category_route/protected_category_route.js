7d4a0a6b139a1a02516b5c27e13cef008ee70630
Anmewzaa <punyakon857@gmail.com>
2024-06-07 19:12:59 +0700

    Update protected_category_route.js

--- project/routes/category_route/protected_category_route.js
+++ project/routes/category_route/protected_category_route.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { create_category } = require("../../controllers/category_controller");
+const {
+  create_category,
+  update_category,
+  delete_category,
+} = require("../../controllers/category_controller");
 
 // CREATE
 router.post("/create", create_category);
 // UPDATE
-
+router.put("/update/:category_id", update_category);
 // DELETE
+router.delete("/delete/:category_id", delete_category);
 
 module.exports = router;
