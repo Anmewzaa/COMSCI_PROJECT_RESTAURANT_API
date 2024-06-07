0e6e7cf1e13f17b7da7486dedb3e5c3a43b301b8
Anmewzaa <punyakon857@gmail.com>
2024-06-07 19:13:02 +0700

    Update unportected_option_route.js

--- project/routes/option_route/unportected_option_route.js
+++ project/routes/option_route/unportected_option_route.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { get_option } = require("../../controllers/option_controller");
+const {
+  get_option,
+  getone_option,
+} = require("../../controllers/option_controller");
 
 // GET
 router.get("/get", get_option);
+router.get("/get/:option_id", getone_option);
 
 module.exports = router;
