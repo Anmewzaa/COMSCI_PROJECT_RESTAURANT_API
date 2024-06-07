bab0739de636a53de805a034f2333522e91f6aa0
Anmewzaa <punyakon857@gmail.com>
2024-06-07 17:48:04 +0700

    Update menu_controller.js

--- project/controllers/menu_controller.js
+++ project/controllers/menu_controller.js
@@ -4,12 +4,34 @@ const MenuModel = require("../models/menu_model");
 // UNPROTECTED
 exports.get_menu = async (req, res) => {
   try {
-    await MenuModel.find({}).then((data) => {
-      res.status(200).json({
-        response: [data],
-        error: "",
+    await MenuModel.find({})
+      .populate("category_id")
+      .populate("menu_option")
+      .then((data) => {
+        res.status(200).json({
+          response: [data],
+          error: "",
+        });
       });
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
     });
+  }
+};
+exports.getone_menu = async (req, res) => {
+  try {
+    const { menu_id } = req.params;
+    await MenuModel.findOne({ menu_id })
+      .populate("category_id")
+      .populate("menu_option")
+      .then((data) => {
+        res.status(200).json({
+          response: [data],
+          error: "",
+        });
+      });
   } catch (err) {
     res.json({
       response: [],
@@ -27,7 +49,43 @@ exports.create_menu = async (req, res) => {
       menu_describe_th,
       menu_describe_en,
       menu_price,
+      category_id,
+      menu_option,
     } = req.body;
+    if (
+      !(
+        menu_name_th &&
+        menu_name_en &&
+        menu_describe_th &&
+        menu_describe_en &&
+        menu_price &&
+        category_id &&
+        menu_option &&
+        req.file
+      )
+    ) {
+      return res.json({
+        response: [],
+        error: "input required",
+      });
+    }
+    menu_image = req.file.filename;
+    await MenuModel.create({
+      menu_id: uuidv4(),
+      menu_name_th: menu_name_th,
+      menu_name_en: menu_name_en,
+      menu_describe_th: menu_describe_th,
+      menu_describe_en: menu_describe_en,
+      menu_price: menu_price,
+      menu_image: menu_image,
+      category_id: category_id,
+      menu_option: menu_option,
+    }).then(() => {
+      res.status(201).json({
+        response: [{ message: "create menu success" }],
+        error: "",
+      });
+    });
   } catch (err) {
     res.json({
       response: [],
