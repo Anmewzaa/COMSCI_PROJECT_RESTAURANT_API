3c701929972af5aac97a39188a06a237912612aa
Anmewzaa <punyakon857@gmail.com>
2024-06-07 19:13:00 +0700

    Update protected_option_route.js

--- project/routes/option_route/protected_option_route.js
+++ project/routes/option_route/protected_option_route.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { create_option } = require("../../controllers/option_controller");
+const {
+  create_option,
+  update_option,
+  delete_option,
+} = require("../../controllers/option_controller");
 
 // CREATE
 router.post("/create", create_option);
 // UPDATE
-
+router.put("/update/:option_id", update_option);
 // DELETE
+router.delete("/delete/:option_id", delete_option);
 
 module.exports = router;
