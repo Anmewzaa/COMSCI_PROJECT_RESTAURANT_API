be373cf2e659867ff7f49d23fadff239a38d1b1c
Anmewzaa <punyakon857@gmail.com>
2024-06-07 17:48:22 +0700

    Update unprotected_menu.js

--- project/routes/menu_route/unprotected_menu.js
+++ project/routes/menu_route/unprotected_menu.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { get_menu } = require("../../controllers/menu_controller");
+const { get_menu, getone_menu } = require("../../controllers/menu_controller");
 
 // GET
 router.get("/get", get_menu);
+router.get("/get/:menu_id", getone_menu);
 
 module.exports = router;
