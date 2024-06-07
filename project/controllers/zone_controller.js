423459335aaa78c277b4cfc1c6bd521f10455a41
Anmewzaa <punyakon857@gmail.com>
2024-06-07 17:48:05 +0700

    Create zone_controller.js

---
+++ project/controllers/zone_controller.js
@@ -0,0 +1,85 @@
+const { v4: uuidv4 } = require("uuid");
+const ZoneModel = require("../models/zone_model");
+
+// PROTECTED
+exports.get_zone = async (req, res) => {
+  try {
+    await ZoneModel.find({}).then((data) => {
+      res.json({
+        response: [data],
+        error: "",
+      });
+    });
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
+exports.create_zone = async (req, res) => {
+  try {
+    const { zone_name } = req.body;
+    if (!zone_name) {
+      return res.status(400).json({
+        response: [],
+        error: "input required",
+      });
+    }
+    await ZoneModel.create({ zone_id: uuidv4(), zone_name: zone_name }).then(
+      () => {
+        res.json({
+          response: [{ message: "create zone success" }],
+          error: "",
+        });
+      }
+    );
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
+exports.update_zone = async (req, res) => {
+  try {
+    const { zone_name } = req.body;
+    if (!zone_name) {
+      return res.status(400).json({
+        response: [],
+        error: "input required",
+      });
+    }
+    const { zone_id } = req.params;
+    await ZoneModel.findOneAndUpdate(
+      { zone_id: zone_id },
+      { zone_name: zone_name }
+    ).then(() => {
+      res.json({
+        response: [{ message: "update zone success" }],
+        error: "",
+      });
+    });
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
+exports.delete_zone = async (req, res) => {
+  try {
+    const { zone_id } = req.params;
+    await ZoneModel.findOneAndDelete({ zone_id: zone_id }).then(() => {
+      res.json({
+        response: [{ message: "delete zone success" }],
+        error: "",
+      });
+    });
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
