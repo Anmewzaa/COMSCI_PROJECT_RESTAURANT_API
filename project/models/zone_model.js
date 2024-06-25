ef8d2bae99207a5dff0c8d1ebd090ee03ffb4843
Anmewzaa <punyakon857@gmail.com>
2024-06-25 15:45:13 +0700

    Update zone_model.js

--- project/models/zone_model.js
+++ project/models/zone_model.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const zoneSchema = mongoose.Schema({
-  zone_id: {
-    type: String,
-    require: true,
-    unique: true,
+const zoneSchema = mongoose.Schema(
+  {
+    zone_id: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    zone_name: {
+      type: String,
+      require: true,
+    },
   },
-  zone_name: {
-    type: String,
-    require: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Zone", zoneSchema);
