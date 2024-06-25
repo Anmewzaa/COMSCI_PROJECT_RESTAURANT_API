1d0fd30cf186eda4f727094ba90be3ebe14fc435
Anmewzaa <punyakon857@gmail.com>
2024-06-25 15:45:16 +0700

    Update table_models.js

--- project/models/table_models.js
+++ project/models/table_models.js
@@ -1,11 +1,65 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const zones = require("./zone_model");
+const users = require("./user_model");
+const menus = require("./menu_model");
 
-const tableSchema = mongoose.Schema({
-  table_id: {
-    type: String,
-    require: true,
-    unique: true,
+const tableSchema = mongoose.Schema(
+  {
+    table_id: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    table_number: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    table_seat: {
+      type: String,
+      require: true,
+    },
+    table_zone: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: zones,
+        },
+      ],
+      require: true,
+    },
+    table_status: {
+      type: String,
+      require: true,
+    },
+    table_employee: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: users,
+        },
+      ],
+    },
+    table_customer_amount: {
+      type: String,
+    },
+    table_order: {
+      type: [
+        {
+          menu: {
+            type: Schema.Types.ObjectId,
+            ref: menus,
+          },
+          amount: {
+            type: String,
+          },
+          option: [],
+        },
+      ],
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Table", tableSchema);
