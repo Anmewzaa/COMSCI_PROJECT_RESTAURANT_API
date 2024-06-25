4dc05737af37d2ceb83914e865949230dd864b5b
Anmewzaa <punyakon857@gmail.com>
2024-06-25 15:45:18 +0700

    Update option_model.js

--- project/models/option_model.js
+++ project/models/option_model.js
@@ -1,34 +1,42 @@
 const mongoose = require("mongoose");
 
-const optionSchema = mongoose.Schema({
-  option_id: {
-    type: String,
-    require: true,
-    unique: true,
-  },
-  option_name_th: {
-    type: String,
-    require: true,
-  },
-  option_name_en: {
-    type: String,
-    require: true,
-  },
-  sub_option: {
-    type: [
-      {
-        sub_option_name_th: {
+const optionSchema = mongoose.Schema(
+  {
+    option_id: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    option_name: {
+      type: {
+        thai: {
           type: String,
-          require: true,
         },
-        sub_option_name_en: {
+        english: {
           type: String,
-          require: true,
         },
       },
-    ],
-    require: true,
+      require: true,
+    },
+    sub_option: {
+      type: [
+        {
+          sub_option_name: {
+            type: {
+              thai: {
+                type: String,
+              },
+              english: {
+                type: String,
+              },
+            },
+          },
+        },
+      ],
+      require: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Option", optionSchema);
