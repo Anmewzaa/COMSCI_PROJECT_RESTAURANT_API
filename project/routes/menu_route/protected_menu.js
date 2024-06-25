d622206ced7a8a64e9a19e1a1ac4fe1e3c1f1659
Anmewzaa <punyakon857@gmail.com>
2024-06-25 15:45:11 +0700

    Update protected_menu.js

--- project/routes/menu_route/protected_menu.js
+++ project/routes/menu_route/protected_menu.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { create_menu } = require("../../controllers/menu_controller");
+const {
+  create_menu,
+  update_menu,
+  delete_menu,
+} = require("../../controllers/menu_controller");
 const { upload } = require("../../middlewares/multer");
 
 // CREATE
 router.post("/create", upload, create_menu);
-// UPDATE
-
+// UPDATE  !! NEED FIX !!
+router.put("/update/:menu_id", update_menu);
 // DELETE
+router.delete("/delete/:menu_id", delete_menu);
 
 module.exports = router;
