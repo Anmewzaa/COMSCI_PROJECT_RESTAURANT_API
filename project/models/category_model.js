e24ddf9695d816fcb46cecc6bccd5bc0c190369b
Anmewzaa <punyakon857@gmail.com>
2024-06-25 15:45:23 +0700

    Update category_model.js

--- project/models/category_model.js
+++ project/models/category_model.js
@@ -1,19 +1,25 @@
 const mongoose = require("mongoose");
 
-const categorySchema = mongoose.Schema({
-  category_id: {
-    type: String,
-    require: true,
-    unique: true,
+const categorySchema = mongoose.Schema(
+  {
+    category_id: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    category_name: {
+      type: {
+        thai: {
+          type: String,
+        },
+        english: {
+          type: String,
+        },
+      },
+      require: true,
+    },
   },
-  category_name_th: {
-    type: String,
-    require: true,
-  },
-  category_name_en: {
-    type: String,
-    require: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Category", categorySchema);
