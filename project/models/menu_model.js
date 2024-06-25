0cb55cd7a52cb4200d1e6afed8ac1c96fef97e25
Anmewzaa <punyakon857@gmail.com>
2024-06-25 15:45:20 +0700

    Update menu_model.js

--- project/models/menu_model.js
+++ project/models/menu_model.js
@@ -3,53 +3,62 @@ const { Schema } = mongoose;
 const categories = require("./category_model");
 const options = require("./option_model");
 
-const menuSchema = mongoose.Schema({
-  menu_id: {
-    type: String,
-    require: true,
-    unique: true,
-  },
-  menu_name_th: {
-    type: String,
-    require: true,
-  },
-  menu_name_en: {
-    type: String,
-    require: true,
-  },
-  menu_describe_th: {
-    type: String,
-    require: true,
-  },
-  menu_describe_en: {
-    type: String,
-    require: true,
-  },
-  menu_price: {
-    type: String,
-    require: true,
-  },
-  menu_image: {
-    type: String,
-    require: true,
-  },
-  category_id: {
-    type: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: categories,
+const menuSchema = mongoose.Schema(
+  {
+    menu_id: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    menu_name: {
+      type: {
+        thai: {
+          type: String,
+        },
+        english: {
+          type: String,
+        },
       },
-    ],
-    require: true,
-  },
-  menu_option: {
-    type: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: options,
+      require: true,
+    },
+    menu_describe: {
+      type: {
+        thai: {
+          type: String,
+        },
+        english: {
+          type: String,
+        },
       },
-    ],
+      require: true,
+    },
+    menu_price: {
+      type: String,
+      require: true,
+    },
+    menu_image: {
+      type: String,
+      require: true,
+    },
+    menu_category_id: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: categories,
+        },
+      ],
+      require: true,
+    },
+    menu_option_id: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: options,
+        },
+      ],
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Menu", menuSchema);
