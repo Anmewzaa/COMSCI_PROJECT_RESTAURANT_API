9b8b670186d0e92c277bcfce51fb8d5c3c25e6a6
Anmewzaa <punyakon857@gmail.com>
2024-06-25 15:45:29 +0700

    Create table_controller.js

---
+++ project/controllers/table_controller.js
@@ -0,0 +1,211 @@
+const { v4: uuidv4 } = require("uuid");
+const TableModel = require("../models/table_models");
+
+// UNPROTECTED
+exports.get_table = async (req, res) => {
+  try {
+    await TableModel.find({})
+      .populate("table_zone")
+      .populate("table_employee")
+      .populate("table_order.menu")
+      .then((data) => {
+        res.status(200).json({
+          response: [data],
+          error: "",
+        });
+      });
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
+
+// PROTECTED
+exports.create_table = async (req, res) => {
+  try {
+    const { table_number, table_seat, table_zone } = req.body;
+    if (!(table_number && table_seat && table_zone)) {
+      return res.status(400).json({
+        response: [],
+        error: `Input required`,
+      });
+    }
+    var _table = await TableModel.findOne({ table_number: table_number });
+    if (_table) {
+      return res.status(400).json({
+        response: [],
+        error: `This table number is already taken`,
+      });
+    }
+    await TableModel.create({
+      table_id: uuidv4(),
+      table_number: table_number,
+      table_seat: table_seat,
+      table_zone: table_zone,
+      table_status: "close",
+      table_employee: [],
+      table_customer_amount: "",
+      table_order: [],
+    }).then(() => {
+      res.status(201).json({
+        response: [
+          {
+            message: "create table success",
+          },
+        ],
+        error: ``,
+      });
+    });
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
+exports.open_table = async (req, res) => {
+  try {
+    const { table_id } = req.params;
+    const { table_employee, table_customer_amount } = req.body;
+    if (!(table_employee && table_customer_amount)) {
+      return res.status(400).json({
+        response: [],
+        error: `Input required`,
+      });
+    }
+    var _table = await TableModel.findOne({ table_id: table_id });
+    console.log(_table);
+    if (_table === null) {
+      return res.status(400).json({
+        response: [],
+        error: `Invalid table number`,
+      });
+    }
+    await TableModel.findOneAndUpdate(
+      { table_id: table_id },
+      {
+        table_status: "open",
+        table_employee: table_employee,
+        table_customer_amount: table_customer_amount,
+      }
+    ).then(() => {
+      res.status(200).json({
+        response: [{ message: "open table success" }],
+        error: "",
+      });
+    });
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
+exports.close_table = async (req, res) => {
+  try {
+    const { table_id } = req.params;
+    var _table = await TableModel.findOne({ table_id: table_id });
+    console.log(_table);
+    if (_table === null) {
+      return res.status(400).json({
+        response: [],
+        error: `Invalid table number`,
+      });
+    }
+    await TableModel.findOneAndUpdate(
+      { table_id: table_id },
+      {
+        table_status: "close",
+        table_employee: [],
+        table_customer_amount: "",
+        table_order: [],
+      }
+    );
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
+exports.add_order_table = async (req, res) => {
+  try {
+    const { table_id } = req.params;
+    const { table_order } = req.body;
+    if (!(table_order.length !== 0)) {
+      return res.status(400).json({
+        response: [],
+        error: `Input required`,
+      });
+    }
+    var _table = await TableModel.findOne({ table_id: table_id });
+    if (_table === null) {
+      return res.status(400).json({
+        response: [],
+        error: `Invalid table number`,
+      });
+    }
+    await TableModel.findOneAndUpdate(
+      { table_id: table_id },
+      {
+        $push: { table_order: table_order },
+      }
+    ).then(() => {
+      res.status(200).json({
+        response: [{ message: "add order success" }],
+        error: "",
+      });
+    });
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
+exports.edit_order_table = async (req, res) => {
+  try {
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
+exports.remove_order_table = async (req, res) => {
+  try {
+    const { table_id } = req.params;
+    const { id } = req.body;
+    if (!id) {
+      return res.status(400).json({
+        response: [],
+        error: `Input required`,
+      });
+    }
+    var _table = await TableModel.findOne({ table_id: table_id });
+    if (_table === null) {
+      return res.status(400).json({
+        response: [],
+        error: `Invalid table number`,
+      });
+    }
+    await TableModel.findOneAndUpdate(
+      { table_id: table_id },
+      {
+        $pull: { table_order: { _id: id } },
+      }
+    ).then(() => {
+      res.status(200).json({
+        response: [{ message: "remove order success" }],
+        error: "",
+      });
+    });
+  } catch (err) {
+    res.json({
+      response: [],
+      error: `${err}`,
+    });
+  }
+};
