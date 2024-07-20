c10477e209247ed1082c119a43514dca0953ad09
Anmewzaa <punyakon857@gmail.com>
2024-07-20 17:09:16 +0700

    Update unprotected_table.js

--- project/routes/table_route/unprotected_table.js
+++ project/routes/table_route/unprotected_table.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { get_table } = require("../../controllers/table_controller");
+const {
+  get_table,
+  getone_table,
+} = require("../../controllers/table_controller");
 
 // GET
 router.get("/get", get_table);
+router.get("/get/:id", getone_table);
 
 module.exports = router;
