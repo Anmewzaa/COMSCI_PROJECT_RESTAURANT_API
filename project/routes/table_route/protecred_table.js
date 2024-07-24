e1aecb767bfdba03b71a4ec09a2cc1762ae61bbb
Anmewzaa <punyakon857@gmail.com>
2024-07-24 15:02:43 +0700

    Update protecred_table.js

--- project/routes/table_route/protecred_table.js
+++ project/routes/table_route/protecred_table.js
@@ -7,13 +7,15 @@ const {
   close_table,
   add_order_table,
   remove_order_table,
+  change_status_order_table,
 } = require("../../controllers/table_controller");
 
 // POST
 router.post("/create", create_table);
-router.put("/open/:table_id", open_table);
-router.put("/close/:table_id", close_table);
-router.put("/add/:table_id", add_order_table);
+router.put("/open/:_id", open_table);
+router.put("/close/:_id", close_table);
+router.put("/add/:_id", add_order_table);
 router.put("/delete/:table_id", remove_order_table);
+router.put("/change_status/:_id", change_status_order_table);
 
 module.exports = router;
