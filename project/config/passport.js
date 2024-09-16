ff618cfea674b76d5d9cffce83ccc589833665e8
Anmewzaa <punyakon857@gmail.com>
2024-09-16 23:35:42 +0700

    Update passport.js

--- project/config/passport.js
+++ project/config/passport.js
@@ -1,13 +1,11 @@
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const UserModel = require("../models/user_model");
-require("dotenv").config();
 
 module.exports = (passport) => {
-  console.log("JWT Secret:", process.env.JWT_SECRET);
   var opts = {};
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-  opts.secretOrKey = process.env.JWT_SECRET;
+  opts.secretOrKey = process.env.JWT_SECRET || "Punyakon3";
 
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
