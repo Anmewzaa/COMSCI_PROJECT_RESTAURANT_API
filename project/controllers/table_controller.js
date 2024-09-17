e58e55ca3abb4b48064f9490cc9da0a3639497a6
Anmewzaa <punyakon857@gmail.com>
2024-09-17 13:22:39 +0700

    Update table_controller.js

--- project/controllers/table_controller.js
+++ project/controllers/table_controller.js
@@ -24,7 +24,7 @@ exports.get_table = async (req, res) => {
 exports.getone_table = async (req, res) => {
   try {
     const { id } = req.params;
-    await TableModel.findOne({ table_id: table_id })
+    await TableModel.findOne({ table_id: id })
       .populate("table_zone")
       .populate("table_employee")
       .populate("table_order.menu")
